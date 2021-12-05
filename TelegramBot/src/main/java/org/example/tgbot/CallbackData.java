e773f0cadf991a9d35852be77ab83b65bcebc652
WaJIdemar <zavlov_rf@mail.ru>
2021-12-05 16:18:39 +0500

    deploy

--- TelegramBot/src/main/java/org/example/tgbot/CallbackData.java
+++
@@ -1,7 +0,0 @@
-package org.example.tgbot;
-
-public class CallbackData {
-    public String accept = "accept";
-    public String decline = "decline";
-    public String ban = "ban";
-}
--- TelegramBot/src/main/java/org/example/tgbot/ChatClient.java
+++
@@ -1,9 +0,0 @@
-package org.example.tgbot;
-
-public interface ChatClient {
-    Long getModeratorGroupId();
-    
-    void sendMessage(Long chatId, String text, Keyboard keyboard);
-
-    void sendMessageModeratorGroup(TermDefinition termDefinition, InlineKeyboard keyboard);
-}
